67397f0cf246438fa4da8cf948a2b21b3ab19242
himanshu <ui.dev1@notenetic.com>
2026-01-29 21:43:14 +0530

    Changed the design of the Loader according to user requirement

--- frontend/src/Loader/Loader.jsx
+++ frontend/src/Loader/Loader.jsx
@@ -2,123 +2,99 @@ import styled from "styled-components";
 
 const Loader = () => {
   return (
-    <StyledWrapper>
-      <div className="section-center">
-        <div className="section-path">
-          <div className="globe">
-            <div className="wrapper">
-              {Array.from({ length: 16 }).map((_, i) => (
-                <span key={i}></span>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-    </StyledWrapper>
+    <Wrapper>
+      <div className="loader"></div>
+    </Wrapper>
   );
 };
 
-const StyledWrapper = styled.div`
+const Wrapper = styled.div`
   position: fixed;
   inset: 0;
   display: flex;
   align-items: center;
   justify-content: center;
-  background: transparent;
   z-index: 9999;
 
-  .section-center {
-    width: 100%;
-    text-align: center;
-  }
-
-  .section-path {
+  .loader {
     position: relative;
-    width: 238px;
-    height: 76px;
-    border-radius: 35px;
-    margin: 0 auto;
-    background-color: #e6e6e6;
-    box-shadow: inset -2px 20px 10px rgba(0,0,0,.06),
-                inset -2px 30px 10px rgba(0,0,0,.04);
-    border: 3px groove rgba(225,225,225,0.07);
-    overflow: hidden;
+    width: 2.5em;
+    height: 2.5em;
+    transform: rotate(165deg);
   }
 
-  .globe {
-    position: relative;
-    width: 66px;
-    height: 66px;
-    margin: 2px;
-    border-radius: 50%;
-    overflow: hidden;
-    box-shadow: 0 10px 40px rgba(0,0,0,0.65);
-    animation: rotateBall 4s ease infinite;
-  }
-
-  @keyframes rotateBall {
-    0% { transform: translateX(0); }
-    50% { transform: translateX(162px); }
-    100% { transform: translateX(0); }
-  }
-
-  .globe::before {
+  .loader::before,
+  .loader::after {
     content: "";
     position: absolute;
-    inset: 0;
-    border-radius: 50%;
-    box-shadow: inset 0 0 15px #1a252f;
-    opacity: 0.4;
-    z-index: 1;
+    top: 50%;
+    left: 50%;
+    display: block;
+    width: 0.5em;
+    height: 0.5em;
+    border-radius: 0.25em;
+    transform: translate(-50%, -50%);
   }
 
-  .globe::after {
-    content: "";
-    position: absolute;
-    width: 5px;
-    height: 12px;
-    left: 40px;
-    top: 15px;
-    border-radius: 50%;
-    background: rgba(255,255,255,0.1);
-    box-shadow: 0 0 14px 7px rgba(255,255,255,0.1);
-    z-index: 2;
+  .loader::before {
+    animation: before8 2s infinite;
   }
 
-  .wrapper {
-    position: absolute;
-    width: 528px;
-    height: 528px;
-    top: 0;
-    left: -462px;
-    animation: moveBall 4s ease infinite;
+  .loader::after {
+    animation: after6 2s infinite;
   }
 
-  @keyframes moveBall {
-    0% { left: -462px; }
-    50% { left: 0; }
-    100% { left: -462px; }
-  }
-
-  .wrapper span {
-    position: absolute;
-    width: 33px;
-    height: 528px;
-    background: #5c477d;
-    box-shadow: inset 0 0 25px #5c487c;
-  }
-
-  .wrapper span:nth-child(even) {
-    background: #503e6d;
-  }
-
-  ${Array.from({ length: 16 }).map(
-    (_, i) => `
-    .wrapper span:nth-child(${i + 1}) {
-      left: ${i * 33}px;
+  @keyframes before8 {
+    0% {
+      width: 0.5em;
+      box-shadow:
+        1em -0.5em rgba(225, 20, 98, 0.75),
+       -1em  0.5em rgba(111, 202, 220, 0.75);
     }
-  `
-  )}
+    35% {
+      width: 2.5em;
+      box-shadow:
+        0 -0.5em rgba(225, 20, 98, 0.75),
+        0  0.5em rgba(111, 202, 220, 0.75);
+    }
+    70% {
+      width: 0.5em;
+      box-shadow:
+       -1em -0.5em rgba(225, 20, 98, 0.75),
+        1em  0.5em rgba(111, 202, 220, 0.75);
+    }
+    100% {
+      box-shadow:
+        1em -0.5em rgba(225, 20, 98, 0.75),
+       -1em  0.5em rgba(111, 202, 220, 0.75);
+    }
+  }
+
+  @keyframes after6 {
+    0% {
+      height: 0.5em;
+      box-shadow:
+        0.5em  1em rgba(61, 184, 143, 0.75),
+       -0.5em -1em rgba(233, 169, 32, 0.75);
+    }
+    35% {
+      height: 2.5em;
+      box-shadow:
+        0.5em 0 rgba(61, 184, 143, 0.75),
+       -0.5em 0 rgba(233, 169, 32, 0.75);
+    }
+    70% {
+      height: 0.5em;
+      box-shadow:
+        0.5em -1em rgba(61, 184, 143, 0.75),
+       -0.5em  1em rgba(233, 169, 32, 0.75);
+    }
+    100% {
+      box-shadow:
+        0.5em  1em rgba(61, 184, 143, 0.75),
+       -0.5em -1em rgba(233, 169, 32, 0.75);
+    }
+  }
 `;
 
 export default Loader;
