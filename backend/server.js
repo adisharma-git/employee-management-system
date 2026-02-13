a0f94fcae9f002ae77b93b02ab2ccb781274964b
adisharma-git <143196543+adisharma-git@users.noreply.github.com>
2026-02-13 18:54:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,8 @@ const corsOptions = {
   origin: [
     'http://localhost:5173',
     'http://localhost:5000',
-    'https://employee-management-system-xi-nine.vercel.app/landingPage',
-    'https://employee-management-system-mkwu.onrender.com', 
+    'https://employee-management-system-xi-nine.vercel.app',
+    'https://employee-management-system-9qg1.onrender.com', 
     process.env.FRONTEND_URL // We'll add this env variable
   ].filter(Boolean), // Remove undefined values
   credentials: true,
