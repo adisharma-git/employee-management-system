e3293dc3ad2bdb7c5b27b9c4cdc80cad83f84441
Himanshu <ui.dev1@notenetic.com>
2026-02-14 17:08:54 +0530

    Added Auto Generated Electronic Mail for Welcome Page

--- frontend/src/pages/MainPageLanding.jsx
+++ frontend/src/pages/MainPageLanding.jsx
@@ -1,11 +1,45 @@
 import { useEffect, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { useNavigate } from "react-router-dom";
 
 export default function MainPageLanding() {
   const [isVisible, setIsVisible] = useState(false);
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     setIsVisible(true);
   }, []);
+
+  const handleSubmit = async () => {
+    if (!email) {
+      alert("Please enter your email");
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      await emailjs.send(
+        "service_ipl26up",   
+        "template_2qlv5ud", 
+        {
+          user_email: email,
+        },
+        "uF9BCZTT64heEVjiT"  
+      );
+
+  
+      navigate("/thank-you");  
+    } catch (error) {
+      console.error(error);
+      alert("Something went wrong ❌");
+    }
+
+    setLoading(false);
+  };
 
   return (
     <section className="min-h-screen bg-white pt-36 pb-24 px-6 flex items-center justify-center">
@@ -21,6 +55,7 @@ export default function MainPageLanding() {
               Management <span className="text-[#021f54]">Platform</span>
             </span>
           </h1>
+
           <p className="text-gray-600 max-w-2xl mx-auto mb-12 text-base sm:text-lg leading-relaxed">
             Manage attendance, payroll, leaves, and performance with ease using
             our secure and scalable employee management system.
@@ -29,21 +64,17 @@ export default function MainPageLanding() {
           <div className="flex flex-col sm:flex-row items-center justify-center gap-4 max-w-xl mx-auto">
             <input
               type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               placeholder="Enter your work email"
-              className="
-                w-full
-                px-5 py-4
-                text-sm sm:text-base
-                border
-                rounded-xl
-                focus:outline-none
-                focus:ring-2
-                focus:#021f54
-              "
+              className="w-full px-5 py-4 text-sm sm:text-base border rounded-xl focus:outline-none focus:ring-2 focus:ring-[#021f54]"
             />
 
-            <button className="w-full bg-[#021f54] text-white py-3 rounded-lg font-semibold hover:opacity-90">
-              Get a Quote!
+            <button
+              onClick={handleSubmit}
+              className="w-full bg-[#021f54] text-white py-4 rounded-xl font-semibold hover:opacity-90 transition"
+            >
+              {loading ? "Sending..." : "Get a Quote!"}
             </button>
           </div>
         </div>
