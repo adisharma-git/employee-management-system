4f4ab3ae60aae79ade256c7a924677bc590f7ed6
Himanshu <ui.dev1@notenetic.com>
2026-03-11 23:58:10 +0530

    Implement lazy loading across the app to reduce load time

--- frontend/src/LeavesSection/LeaveForm.jsx
+++ frontend/src/LeavesSection/LeaveForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import DatePicker from "react-datepicker";
+
 import "react-datepicker/dist/react-datepicker.css";
 import { format } from "date-fns";
 import api from "../api/axios";
